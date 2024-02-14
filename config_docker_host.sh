cc914e7126e705378a754662f3a5b7bac527a1e5
Doron Gombosh <gombosh@gmail.com>
2024-02-14 16:14:53 +0200

    Update config_docker_host.sh

--- config_docker_host.sh
+++ config_docker_host.sh
@@ -1,8 +1,8 @@
 #todo add check if $1 exists
 echo "START"
-echo '{"insecure-registries" : [ "' > /etc/docker/daemon.json
-echo $1 >> /etc/docker/daemon.json
-echo '" ]}' >> /etc/docker/daemon.json
+echo '{"insecure-registries" : [' > /etc/docker/daemon.json
+echo \t\"$1\" >> /etc/docker/daemon.json
+echo ']}' >> /etc/docker/daemon.json
 systemctl restart docker.service
 sudo -u $SUDO_USER docker login $1
 echo "DONE"
